badac9d406aaad5c3271b2e9beef6d5a75fabeca
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-26 23:24:29 +0300

    c

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElem = document.querySelector('#root')
+
+const renderSeconds = time => {
+    const seconds = new Date(time).getSeconds();
+    const backgroundColor = seconds % 2 === 0
+        ? '#fff'
+        : '#000';
+    const textColor = seconds % 2 !== 0
+        ? '#fff'
+        : '#000';
+    const styles = {
+        color: textColor,
+        backgroundColor
+    }
+
+    const element = (
+        <div className="seconds" style={styles}>
+            {seconds}
+        </div>
+    );
+
+    ReactDOM.render(element, rootElem);
+}
+
+renderSeconds(new Date());
+
+setInterval(() => renderSeconds(new Date()), 1000);
