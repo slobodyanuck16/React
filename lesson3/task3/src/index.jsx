f7eb560aa03140a8aef82f3ab3ed47f6038608ff
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-27 14:36:52 +0300

    c

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Comment from './Comment';
+
+const rootElem = document.querySelector('#root');
+
+const user ={
+    name: 'Tom',
+    avatarUrl: 'https://avatars3.githubusercontent.com/u10001'
+  }
+
+ReactDOM.render(
+    <Comment
+      author={user}
+      text="Good job"
+      date={new Date()}
+    />,
+    rootElem
+);
