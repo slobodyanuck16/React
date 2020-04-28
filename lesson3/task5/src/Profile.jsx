82386cb912224cd9ebd8f1417bf1c27e857d362d
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-28 10:38:08 +0300

    c

---
+++ lesson3/task5/src/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import moment from "moment";
+
+const formatDate = (date) => moment(date).format("DD MMM YYYY");
+
+const Profile = (props) => {
+    return (
+        <>
+            <div className="profile">
+                {`${props.userData.firstName} ${props.userData.lastName}`}
+                <div>
+                    {`Was born ${formatDate(props.userData.birthDate)}
+                 in ${props.userData.birthPlace}`}
+                </div>
+            </div>
+        </>
+    );
+};
+export default Profile;
