f1001ba20df406ee8443d664997b364de412c3d6
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-28 14:34:59 +0300

    c

--- lesson4/task1/src/Counter.jsx
+++ lesson4/task1/src/Counter.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import './counter.scss';
 
-let counter = 0;
-
 class Counter extends Component {
     constructor() {
         super();
         this.state = {
-            counter: 0,
+            counter: 17,
         };
 
         //Do not do this way
