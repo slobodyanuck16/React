ea5844ccad81fd78f3414a5fbd96a18af74b730b
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-29 00:33:26 +0300

    c

---
+++ lesson5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Colors from './Colors'
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Colors />, rootElem);
