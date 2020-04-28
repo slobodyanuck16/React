474ab1dedcbf9fc92bc38fd0effa011a05ee2875
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-28 13:09:38 +0300

    c

--- lesson3/task5wtf/src/Profile.jsx
+++ lesson3/task5wtf/src/Profile.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import moment from "moment";
+import React from 'react';
+import moment from 'moment';
 
-const formatDate = (date) => moment(date).format("DD MMM YYYY");
+const formatDate = date => 
+  moment(date).format("DD MMM YY");
 
 const Profile = (props) => {
     return (
         <>
-            <div className="profile">
+            <div className="profile__name">
                 {`${props.userData.firstName} ${props.userData.lastName}`}
-            <div>
+            </div>
+            <div className="profile__birth">
                 {`Was born ${formatDate(props.userData.birthDate)}
                  in ${props.userData.birthPlace}`}
-            </div>
             </div>
         </>
     );
