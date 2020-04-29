f5a471529a193f8b682b260cbc3d3fad729fc237
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-29 20:24:14 +0300

    c

---
+++ lesson6/task4/src/Status.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Online from "./Online";
+import Offline from "./Offline";
+
+const Status = (props) => {
+    if (props.isOnline) return <Online />;
+    return <Offline />;
+};
+
+export default Status;
---
+++ lesson6/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Status from './Status';
+import './index.scss';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Status />, rootElem);
