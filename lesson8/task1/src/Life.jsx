c4c754ce2c063c176bc430d4279eb3fe24f3d928
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-30 19:58:47 +0300

    c

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 
 class Life extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
         console.log('constructor: good place to create state');
     }
+
     componentDidMount() {
         console.log('componentDidMount: API calls, subscriptions');
     }
-    shouldComponentUpdate() {
+
+    shouldComponentUpdate(nextProps, nextState){
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+        return this.props !== nextProps;
     }
-    componentDidUpdate() {
+
+    componentDidUpdate(prevProps, prevState) {
         console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
+
     componentWillUnmount() {
-        console.log('=> componentWillUnmount(): cleanup before DOM related to component will be removed');
+        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
     }
-    
+
     render() {
         console.log('return React element to build DOM');
-        return <span>lol</span>
+        return true;
     }
 }
 
