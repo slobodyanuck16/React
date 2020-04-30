72739601836249c299f6bae309da7cda49e53576
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-30 19:54:00 +0300

    c

---
+++ lesson8/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Life from './Life'
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Life />, rootElem);
