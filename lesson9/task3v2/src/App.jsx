d3c941ca8a51f7ff00f6a7abc879c1844e5c0323
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-05-04 22:12:21 +0300

    c

---
+++ lesson9/task3v2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+const App = () => {
+    return (
+        <UserForm />
+    );
+};
+
+export default App;
---
+++ lesson9/task3v2/src/UserForm.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+class UserForm extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = [...new FormData(this.formRef)].reduce(
+            (acc, [name, value]) => ({ ...acc, [name]: value }),
+            {}
+        );
+
+        this.props.onSubmit(formData);
+    };
+
+    setRef = (node) => {
+        this.formRef = node;
+    };
+
+    render() {
+        return (
+            <form
+                ref={this.setRef}
+                onSubmit={this.handleSubmit}
+                className="login-form"
+            >
+                <h1 className="form-title">Profile</h1>
+                <div className="form-control">
+                    <label className="form-label" htmlFor="name">
+                        Name
+                    </label>
+                    <input
+                        className="form-input"
+                        type="text"
+                        id="name"
+                        name="name"
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="form-label" htmlFor="student">
+                        Student
+                    </label>
+                    <input
+                        className="form-input"
+                        type="checkbox"
+                        id="student"
+                        name="student"
+                    />
+                </div>
+                <div className="form-control">
+                    <label
+                        className="form-label"
+                        id="occupation"
+                        htmlFor="occupation"
+                    >
+                        Occupation
+                    </label>
+                    <select name="occupation" className="form-input">
+                        <option value="london">London</option>
+                        <option value="new-york">New York</option>
+                        <option value="coconut">Sidney</option>
+                        <option value="berlin">Berlin</option>
+                    </select>
+                </div>
+                <div className="form-control">
+                    <label className="form-label" id="about" htmlFor="about">
+                        About
+                    </label>
+                    <textarea name="about" className="form-input" />
+                </div>
+                <button className="submit-button" type="submit">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default UserForm;
