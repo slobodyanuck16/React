3a9adbb088efade0a9fe53d97c81694c6fc08185
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-05-25 16:00:48 +0300

    c

--- lessson13/task3/src/Users.jsx
+++ lessson13/task3/src/Users.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Route, Link, Switch, useParams } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import User from "./User";
 
 function Products({ match }) {
-    console.log(match);
-
     return (
         <div className="page__content">
             <h1>Products</h1>
             <ul className="navigation">
                 <li className="navigation__item">
-                    <Link to={`${match.url}/github`}>GitHub</Link>
+                    <Link to={`${match.url}/github`}>Github</Link>
                 </li>
                 <li className="navigation__item">
-                    <Link to={`${match.url}/facebook`}>FaceBook</Link>
+                    <Link to={`${match.url}/facebook`}>Facebook</Link>
                 </li>
             </ul>
             <Switch>
