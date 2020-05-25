563b834bb67bddb73fbeef5604df40775dd865fb
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-05-25 17:47:00 +0300

    c

--- lessson13/task3v2/src/Home.jsx
+++ lessson13/task3v2/src/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className="page__content">
             <h1>🏠</h1>
-            <Link to="/users">To users</Link>
+            {/* <Link to="/users">To Users</Link> */}
         </div>
     );
 }
