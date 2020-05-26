964e2125f0d41764fc5a73ab9d662bc2bfac09d7
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-05-26 13:04:24 +0300

    c

---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const Dimensions = () => {
+    const [dimensions, setDimensions] = useState({
+        width: null,
+        height: null,
+    });
+
+    useEffect(() => {
+        const { innerHeight, innerWidth } = window;
+        setDimensions({ width: innerWidth, height: innerHeight });
+    });
+
+    const { width, height } = dimensions;
+    return <div class="dimensions">{`${width}px - ${height}px`}</div>;
+};
+
+export default Dimensions;
